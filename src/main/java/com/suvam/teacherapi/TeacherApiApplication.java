d8125bf16fd8edc58315b8fdd658c3f19518dcf7
suvamgurung <gurungsuvam265@gmail.com>
2026-08-12 15:01:17 +0545

    Initial Spring Boot setup

---
+++ src/main/java/com/suvam/teacherapi/TeacherApiApplication.java
@@ -0,0 +1,13 @@
+package com.suvam.teacherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeacherApiApplication.class, args);
+    }
+
+}
