51f59e50c02b5254df7022bcbb1756ae76e48f7c
suvamgurung <gurungsuvam265@gmail.com>
2026-08-13 14:44:33 +0545

    refactor: convert TeacherRepo from class to JpaRepository interface

--- src/main/java/com/suvam/teacherapi/repository/TeacherRepo.java
+++ src/main/java/com/suvam/teacherapi/repository/TeacherRepo.java
@@ -1,4 +1,9 @@
 package com.suvam.teacherapi.repository;
 
-public class TeacherRepo {
+import com.suvam.teacherapi.model.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherRepo extends JpaRepository<Teacher, Long> {
 }
