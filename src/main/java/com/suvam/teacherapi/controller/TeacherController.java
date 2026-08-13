6ac59ca7956215350b0b30643964b7b79b0a22f8
suvamgurung <gurungsuvam265@gmail.com>
2026-08-13 18:40:34 +0545

    add API Versoning

--- src/main/java/com/suvam/teacherapi/controller/TeacherController.java
+++ src/main/java/com/suvam/teacherapi/controller/TeacherController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/teacher")
+@RequestMapping("/api/v1/teachers")
 public class TeacherController {
 
     private final TeacherService service;
