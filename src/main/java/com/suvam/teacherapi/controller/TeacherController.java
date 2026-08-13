4fb06478238700ecc62b0b7990fdda683c1e8a68
suvamgurung <gurungsuvam265@gmail.com>
2026-08-13 12:59:26 +0545

    Create teacher endpoint to add teacher

--- src/main/java/com/suvam/teacherapi/controller/TeacherController.java
+++ src/main/java/com/suvam/teacherapi/controller/TeacherController.java
@@ -1,4 +1,25 @@
 package com.suvam.teacherapi.controller;
 
+import com.suvam.teacherapi.model.Teacher;
+import com.suvam.teacherapi.service.TeacherService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/teacher")
 public class TeacherController {
+
+    private final TeacherService service;
+
+    public TeacherController(TeacherService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> addTeacher(@RequestBody Teacher teacher) {
+        return ResponseEntity.ok(service.addTeacher(teacher));
+    }
 }
--- src/main/java/com/suvam/teacherapi/service/TeacherService.java
+++ src/main/java/com/suvam/teacherapi/service/TeacherService.java
@@ -1,4 +1,12 @@
 package com.suvam.teacherapi.service;
 
+import com.suvam.teacherapi.model.Teacher;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TeacherService {
+
+    public String addTeacher(Teacher teacher) {
+
+    }
 }
