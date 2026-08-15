29d2e08f3cce4340516e06be02fbc3f5e43c1edc
suvamgurung <gurungsuvam265@gmail.com>
2026-08-15 13:56:07 +0545

    Add global handler for type mismatch errors

--- src/main/java/com/suvam/teacherapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/suvam/teacherapi/exception/GlobalExceptionHandler.java
@@ -5,12 +5,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    // Correct endpoint + valid ID but teacher doesn't exist -> Call this exception
     @ExceptionHandler(TeacherNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleTeacherNotFound(
             TeacherNotFoundException exception,
@@ -23,5 +25,21 @@ public class GlobalExceptionHandler {
                 request.getRequestURI()
         );
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    // Correct endpoint + wrong ID type -> Call this exception
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleTypeMismatch(
+            HttpServletRequest request,
+            MethodArgumentTypeMismatchException exception
+    ) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                exception.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
