43abebd69889ee88aee01ea57eba7a154e7c37f5
suvamgurung <gurungsuvam265@gmail.com>
2026-08-15 19:51:37 +0545

    Add extra field that gives list of errors

--- src/main/java/com/suvam/teacherapi/exception/ErrorResponse.java
+++ src/main/java/com/suvam/teacherapi/exception/ErrorResponse.java
@@ -1,11 +1,13 @@
 package com.suvam.teacherapi.exception;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 public record ErrorResponse(
         LocalDateTime timestamp,
         int status,
         String error,
         String message,
+        Map<String, String> errors,
         String path
 ) {}
